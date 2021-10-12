6ede79fd34fcae297d5058f6fa06985fabfca4fb
Sarah Weaver <lwflouisa@gmail.com>
2021-10-11 20:29:44 -0400

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -4,21 +4,28 @@ do_size     = ["tiny", "small", "medium"]
 size_chosen = do_size[number]
 size_limit  = size_chosen.size.to_i
 
-puts "Transmission"
+puts "#{title}"
 
 # Character name chosen from number
 name_number = File.read("data/identity/number.txt").strip.to_i
 
+# Base dialogue sequence on size limit.
 size_limit.times do
+
+  # Username logistics
   name_list   = File.read("data/identity/name_list.txt").split(", ")
   name        = name_list[name_number]
+  name_limit  = name_list[name_number].size.to_i
 
+  # Write date to file.
   system("date > data/date/date.txt)
 
   date = File.read("data/date/date.txt).strip.to_i
 
+  # Print character dialogue to screen.
   print "[#{date} #{name}] "; dialogue = gets.chomp
 
+  # If writer specifies exit then aborts
   if dialogue == "exit"
     abort
   end
