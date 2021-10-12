c894333e797ab965f8a4bb6f08d4d091a6a68c40
Sarah Weaver <lwflouisa@gmail.com>
2021-10-11 20:06:18 -0400

    Create bot.rb

---
+++ bot.rb
@@ -0,0 +1,24 @@
+# Loop limit based on configured screen settings.
+number      = File.read("data/config/line_num.txt").strip.to_i
+do_size     = ["tiny", "small", "medium"]
+size_chosen = do_size[number]
+size_limit  = size_chosen.size.to_i
+
+puts "Transmission"
+
+size_limit.times do
+  # Character name chosen from number
+  name_number = File.read("data/identity/number.txt").strip.to_i
+  name_list   = File.read("data/identity/number.txt").split(", ")
+  name        = name_list[name_number]
+
+  system("date > data/date/date.txt)
+
+  date = File.read("data/date/date.txt).strip.to_i
+
+  print "[#{date} #{name}] "; dialogue = gets.chomp
+
+  if dialogue == "exit"
+    abort
+  end
+end
